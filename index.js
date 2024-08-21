9d486ef408ce3d27d9016fd9f90bdd7b80d1429a
Ivan A Ferreira Aguiar <ivnagst@gmail.com>
2024-08-20 22:29:22 -0300

    gitpod adjustments

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/api/:date?", (req, res) => {
-  const input = req.params.date;
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
+
+app.get("/api/:timestamp", (req, res) => {
+  const input = req.params.timestamp;
 
   const date = input ? new Date(input) : new Date();
 
   // Verifica se a data é válida
-  if (isNaN(date.getTime())) {
-    return res.status(400).json({ error: "Invalid date format" });
+  if (isNaN(Number(timestamp)) && timestamp.length === 13) {
+    return res.json({
+      unix: timestamp,
+      utc: new Date(Number(timestamp)).toUTCString(),
+    });
   }
 
-  const unixTimestamp = date.getTime();
-  const utcDate = date.toUTCString();
-
-  res.json({
-    unix: unixTimestamp,
-    utc: utcDate,
-  });
+  if (new Date(timestamp).toUTCString() !== "Invalid Date") {
+    return res.json({
+      unix: new Date(timestamp).getTime(),
+      utc: timestamp,
+    });
+  }
+  res.send(timestamp);
 });
 
 app.listen(port, () => {
